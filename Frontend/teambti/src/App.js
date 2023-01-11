81bdb83dd4913eb063ad6fdb8da7bc9f8c89e2c1
he-reme <rlagpfla1017@naver.com>
2023-01-11 11:06:47 +0900

    Router 설정 및 페이지 생성
    - Router 설정
    - 페이지 생성 : 로그인, 메인, 업무할당, 1:1성격비교, 마이페이지

--- Frontend/teambti/src/App.js
+++ Frontend/teambti/src/App.js
@@ -1,16 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "./main/Main";
-import Login from "./login/Login";
+
+import Main from "./pages/main/Main";
+import Login from "./pages/login/Login";
+import Assignment from "./pages/assignment/Assignment";
+import Comparison from "./pages/comparison/Comparison";
+import Mypage from "./pages/mypage/Mypage";
+
+import Home from "./pages/home/Home";
+import Test from "./pages/home/Test";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          {/* 로그인화면 */}
           <Route path="/" element={<Login></Login>}></Route>
+
+          {/* 메인화면 */}
           <Route path="/main" element={<Main></Main>}></Route>
+          
+          {/* 업무할당 */}
+          <Route path="/assignment" element={<Assignment></Assignment>}></Route>
+
+          {/* 1:1 성격 비교 */}
+          <Route path="/comparison" element={<Comparison></Comparison>}></Route>
+
+          {/* 마이페이지 */}
+          <Route path="/mypage" element={<Mypage></Mypage>}></Route>
+
+          {/* 여기 밑으로는 테스트 */}
+          <Route path="/home" element={<Home></Home>}></Route>
+          <Route path="/test" element={<Test></Test>}></Route>
+          
         </Routes>
       </BrowserRouter>
     </div>
