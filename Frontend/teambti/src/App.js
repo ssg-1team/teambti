0978330d596cbadcb7b69a644eef2508adf0846d
junojam <junojam0622@gmail.com>
2023-01-11 11:13:20 +0900

    [F] feature_character

--- Frontend/teambti/src/App.js
+++ Frontend/teambti/src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./main/Main";
 import Login from "./login/Login";
+import Character from "./character/Character";
 
 function App() {
   return (
@@ -11,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login></Login>}></Route>
           <Route path="/main" element={<Main></Main>}></Route>
+          <Route path="/character" element={<Character></Character>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
