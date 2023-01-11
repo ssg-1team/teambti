e2b348ab82243876824a928d3a7b7abbeefef1ed
he-reme <rlagpfla1017@naver.com>
2023-01-11 20:14:19 +0900

    Router 설정 고민 필요

--- Frontend/teambti/src/pages/home/Test.js
+++ Frontend/teambti/src/pages/home/Test.js
@@ -5,14 +5,9 @@ import MuiDrawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Link from '@mui/material/Link';
 
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
@@ -24,11 +19,8 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
 
-import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import Home from "./Home";
 
 function Copyright(props) {
   return (
@@ -104,67 +96,6 @@ function DashboardContent() {
     position: "Web",
   }
 
-  const others = [
-    {
-      id: 2,
-      name: "cocoon",
-      type: "INFP",
-      position: "POS",
-    },
-    {
-      id: 3,
-      name: "ultra",
-      type: "ESFP",
-      position: "IOS",
-    },
-    {
-      id: 4,
-      name: "hozae",
-      type: "ISTJ",
-      position: "AOS",
-    },
-    {
-      id: 4,
-      name: "hozae",
-      type: "ISTJ",
-      position: "AOS",
-    },
-    {
-      id: 4,
-      name: "hozae",
-      type: "ISTJ",
-      position: "AOS",
-    },
-    {
-      id: 4,
-      name: "hozae",
-      type: "ISTJ",
-      position: "AOS",
-    },
-    {
-      id: 4,
-      name: "hozae",
-      type: "ISTJ",
-      position: "AOS",
-    },
-    {
-      id: 4,
-      name: "hozae",
-      type: "ISTJ",
-      position: "AOS",
-    },
-  ];
-
-  const settings = {  
-    // 슬라이드 옵션들
-    arrows: true,  // 화살표 표시
-    dots: true,  // 밑에 현재 페이지와 나머지 페이지 점으로 표시
-    infinite: false,  // 무한 반복
-    speed: 500,  // 넘기는 속도
-    slidesToShow: 3,  // 슬라이드에 보여지는 아이템 개수
-    slidesToScroll: 1,  // 슬라이드 넘기는 아이템 개수
-};
-
   return (
     <ThemeProvider theme={mdTheme}>
       <Box sx={{ display: 'flex' }}>
@@ -236,50 +167,14 @@ function DashboardContent() {
               },
             }}
           >
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{ maxWidth: 340 }}>
               <CardMedia
                       component="img"
                       height="450"
                       image="images/characterExample.png"
                       alt="green iguana"
               />    
-            </Card>
-          </Box>
-          <Typography gutterBottom variant="h5" component="div">
-            {user.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {user.type} / {user.position}
-          </Typography>
-          <Button variant="contained" color='inherit' sx={{m:4}}>마이페이지</Button>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          
-          <div>
-            <Slider {...settings}>
-              {others.map((user) => (
-                <div>
-                <Card sx={{ maxWidth: 345 }}>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="450"
-                      image="images/characterExample.png"
-                      alt="green iguana"
-                    />
-                    <CardContent>
+              <CardContent>
                       <Typography gutterBottom variant="h5" component="div">
                         {user.name}
                       </Typography>
@@ -287,16 +182,11 @@ function DashboardContent() {
                         {user.type} / {user.position}
                       </Typography>
                     </CardContent>
-                  </CardActionArea>
-                </Card>
-              </div>
-              ))}
-          
-          </Slider>
-          </div>
-          <Copyright sx={{ pt: 4 }} />
-          
-        </Box>
+            </Card>
+          </Box>
+          <Button variant="contained" color='inherit' sx={{m:4}}>마이페이지</Button>
+        </Drawer>
+        <Home/>
       </Box>
     </ThemeProvider>
   );
@@ -305,4 +195,3 @@ function DashboardContent() {
 export default function Dashboard() {
   return <DashboardContent />;
 }
-
