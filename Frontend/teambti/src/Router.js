9bc38e2b73c6287734efa551138f92f12de3c37e
junojam <nak3652@naver.com>
2023-01-14 14:37:58 +0900

    [F]coworking

--- Frontend/teambti/src/Router.js
+++ Frontend/teambti/src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/main/Main";
 import Login from "./pages/login/Login";
 import Coworking from "./pages/coworking/Coworking";
+import CoworkingStart from "./pages/coworking/CoWorkingStart";
 import Comparison from "./pages/comparison/Comparison";
 import Mypage from "./pages/mypage/Mypage";
 import Character from "./pages/character/Character";
@@ -25,7 +26,10 @@ const Router = ({ isLoggedIn, login }) => {
         <Route path="/characterTest" element={<CharacterTest />}></Route>
         <Route path="/character" exact element={<Character />}></Route>
         {/* 업무할당 */}
-        <Route path="/coworking2" exact element={<Coworking />}></Route>
+        <Route path="/coworking" exact element={<CoworkingStart/>}></Route>
+        <Route path="/coworking3" exact element={<Coworking questionsNumber={3}/>}></Route>
+        <Route path="/coworking5" exact element={<Coworking questionsNumber={5}/>}></Route>
+        <Route path="/coworking7" exact element={<Coworking questionsNumber={7}/>}></Route>
 
         {/* 1:1 성격 비교 */}
         <Route path="/comparison" exact element={<Comparison />}></Route>
